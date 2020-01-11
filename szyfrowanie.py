9574f24c064a57f0f13c227bb6f227d673a5af6a
profsor500 <59374572+profsor500@users.noreply.github.com>
2020-01-11 12:42:13 +0100

    Update szyfrowanie.py

--- szyfrowanie.py
+++ szyfrowanie.py
@@ -1,27 +1,25 @@
-def DecToAny(num, sys):
+import sys
+
+def DecToAny(num,syst):
     mod=[]
     while num!=0:
-        mod.append(int(num % sys))
-        num=int((num-(num % sys))/sys
+        mod.append(num%syst)
+        num=int((num-(num%syst))/syst)
     mod.reverse()
-    return("".join(str(x) for x in mod))
+    return "".join(str(x) for x in mod)
 
 def szyfrowanie(str):
     tab=[]
-    sys = 2
+    syst=2
     for i in range(len(str)):
-        tab.append(DecToAny(ord(str[i]), sys))
-        sys = (ord(str[i]) % 8) + 2
-    print(" ".join(tab))
-    return(" ".join(tab))
-
-def pelne_szyfrowanie(path_to_input, path_to_output):
-    f_in = open(path_to_input,"r")
-    open(path_to_output, 'w').close() #czyści plik przed zapisem, aby zapisać na czysto nowy tekst
-    f_out = open(path_to_output,"a")
+        tab.append(DecToAny(ord(str[i]),syst))
+        syst=(ord(str[i])%8)+2
+    return " ".join(tab)
+def pelne_szyfrowanie(path_input, path_output):
+    f_in=open(path_input,"r")
+    f_out=open(path_output,"w")
     for l in f_in:
         f_out.write(szyfrowanie(l.rstrip())+'\n')
     f_in.close()
     f_out.close()
-
-pelne_szyfrowanie("Input/tekst.txt","Output/Szyfr.txt")
+pelne_szyfrowanie(sys.argv[1],sys.argv[2])
